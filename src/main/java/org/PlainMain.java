5e638c8e46795cbb9799bac79ed3372ded58269e
vremennoe <uliana.zhyla@gmail.com>
2023-01-09 22:52:02 +0200

    hg

---
+++ src/main/java/org/PlainMain.java
@@ -0,0 +1,35 @@
+package org;
+
+public class PlainMain {
+        public static void main (String... args) {
+            Plane.Plane Victory = new Plane.Plane();
+            System.out.println("We have flying on plane " + Victory.PlaneModelName);
+            Victory.fly("Washington");
+            System.out.println("on board we have " + Victory.passenger + " passenger");
+//        System.out.println("Overboard " + Victory.weatherOverboard + " degree");
+            System.out.println("By the way, on our plane Victory we have some famous people : ");
+
+            String[] famousPassengers = new String[5];
+            Victory.setFamousPassengers(famousPassengers); // зачем это нужно делать?//
+            famousPassengers[0] = "Volodymyr Zelensky";
+            famousPassengers[1] = "Kirill Bydanov";
+            famousPassengers[2] = "Valerii Zaluzhniy";
+            famousPassengers[3] = "Denis Prokopenko";
+            famousPassengers[4] = "Vitalii Kim";
+
+            for (int i = 0; i < Victory.getFamousPassengers().length; i++) {
+                System.out.println(famousPassengers[i]);
+            }
+            switch (Victory.getPassenger(0)) { // тут проверяет каждого по индексу, разве это так должно работать?//
+                case "Volodymyr Zelensky":
+                    System.out.println("Wow! It`s President of Ukraine! Amazing!");
+                    break;
+                default:
+                    System.out.println("It`s joke!");
+
+            }
+
+
+        }
+
+    }
---
+++ src/main/java/org/Plane.java
@@ -0,0 +1,37 @@
+package Plane;
+
+public class Plane {
+    public int passenger = 233;
+
+    public String[] getFamousPassengers() {
+        return famousPassengers;
+    }
+    public void setFamousPassengers(String[] famousPassengers) {
+        this.famousPassengers = famousPassengers;
+    }
+
+    String[] famousPassengers = new String[5];
+
+    public String getPassenger (int index) { //зачем создавать это метод?//
+        return famousPassengers[index];
+    }
+    public int weatherOverboard = 18;
+    public void setWeatherOverboard(int weatherOverboard) {
+        this.weatherOverboard = weatherOverboard;
+    }
+
+    public int getPassenger() {
+        return passenger;
+    }
+
+    public String PlaneModelName = "Boeing 777";
+
+    public String getPlainModelName() {
+        return PlaneModelName;
+    }
+
+    public void fly(String direction) {
+        System.out.println(PlaneModelName + " is flying to " + direction);
+    }
+}
+
